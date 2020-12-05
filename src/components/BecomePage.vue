b65b61647a5f92849be81cdde9b3fe8f31684753
rusakov <dan-rusakov@mail.ru>
2020-12-05 17:44:08 +0500

    add become page

---
+++ src/components/BecomePage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="become-page">
+    <h2 class="become-page__title">Наши Санты</h2>
+    <ul class="become-page__santa-list">
+      <li class="become-page__santa-item">
+        <svg class="become-page__santa-item-icon" width="34" height="34">
+          <use xlink:href="#icon-check"></use>
+        </svg>
+        <p class="become-page__santa-name">
+          Денис Васишак
+        </p>
+      </li>
+      <li class="become-page__santa-item">
+        <svg class="become-page__santa-item-icon" width="34" height="34">
+          <use xlink:href="#icon-check"></use>
+        </svg>
+        <p class="become-page__santa-name">
+          Тамара Зыкова
+        </p>
+      </li>
+      <li class="become-page__santa-item">
+        <svg class="become-page__santa-item-icon" width="34" height="34">
+          <use xlink:href="#icon-check"></use>
+        </svg>
+        <p class="become-page__santa-name">
+          Денис Васишак
+        </p>
+      </li>
+      <li class="become-page__santa-item">
+        <svg class="become-page__santa-item-icon" width="34" height="34">
+          <use xlink:href="#icon-check"></use>
+        </svg>
+        <p class="become-page__santa-name">
+          Тамара Зыкова
+        </p>
+      </li>
+    </ul>
+    <button class="btn become-page__btn" type="button">
+      Стать Сантой
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BecomePage',
+}
+</script>
