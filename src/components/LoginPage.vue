fda8757d32628030bc4eed2e34cde49195b17bf3
rusakov <dan-rusakov@mail.ru>
2020-12-05 16:15:28 +0500

    add login page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-page">
+    <h2 class="login-page__title">Вход</h2>
+    <form action="" method="post" class="login-page__form" novalidate>
+      <input
+          aria-label="Email"
+          type="email"
+          name="email"
+          class="text-input login-page__input"
+          placeholder="Email"
+          autocomplete="username"
+      >
+      <input
+          aria-label="Пароль"
+          type="password"
+          name="password"
+          class="text-input login-page__input"
+          placeholder="Пароль"
+          autocomplete="current-password"
+      >
+      <div class="login-page__action-box">
+        <button
+            class="btn login-page__login-btn"
+            type="submit"
+        >Войти</button>
+        <mark class="login-page__action-mark">или</mark>
+        <button
+            class="btn-secondary login-page__registration-page-btn"
+            type="button"
+        >Зарегистрироваться</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+};
+</script>
